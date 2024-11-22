10a44475b5a8044ed7331e8e90404834d7e4877c
inmygod <inmygod@hotmail.com>
2024-11-22 22:20:53 +0600

    Python Variable & Data Types Explained

---
+++ main.py
@@ -0,0 +1,38 @@
+
+# Numeric Data Types:
+age=34 # int
+print(age)
+
+GPA=4.29 # float data type
+print(GPA)
+
+complex_number=(10+20j) # imaginary number
+print(complex_number)
+
+# String types:
+Name="Sabyasachi"
+Country=["-Choose a Country-", "BD", "India", "Russia", "China", "US", "UK"]
+Location=[200, 20.55, "Khulna", "Bangladesh"]
+Alphabet=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q"]
+Names=["John", "Rock", "Jack", "Milton", "Jacob", "Bob", "Rock", "Denver", "David", "George", "Harry", "Denver", "Stephen", "John"]
+print(Name)
+print(Country)
+print(Location)
+print(Alphabet)
+print(Names)
+
+# Tuple Data Types:
+Letter=["A", "B", "C", "D", "E"]
+Letter_and_Numbers=["A", "456", "78.90", "55.34", "A1", "Filter", "Onion", "Crown", "Fruits", "People", ]
+Scrambled_Data=["A", "A", "B", "C", "D", "E", "456", "78.90", "55.34", "A1", "Filter", "Onion", "A1", "A1", "Crown", "Fruits", "People", ]
+print(Letter)
+print(Letter_and_Numbers)
+print(Scrambled_Data)
+
+# Range Data Types:
+numbers=range(1,10)
+numbers2=range(0,34,3)
+numbers3=range(5)
+print(*numbers) # Put a *
+print(*numbers2) # Put a *
+print(*numbers3) # Put a *
